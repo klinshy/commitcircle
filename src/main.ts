c2e583aad262364ace76944204ac226363de5599
Kili S <79143155+klinshy@users.noreply.github.com>
2024-12-18 18:20:46 +0100

    edits on focus areas

--- src/main.ts
+++ src/main.ts
@@ -98,19 +98,31 @@ right: "1vw",
 //// Focus Timer related stuff added by Kilian S.
 async function updateFocusAreas() {
   const focusValue: string = WA.state.focus as string;
-  const focusAreas = ['focusArea'];
+  const focusAreas = ['focusArea', 'focusJitsi'];
 
   for (const areaName of focusAreas) {
       const focusArea = await WA.room.area.get(areaName);
       if (focusArea) {
-          if (focusValue === "0") {
-              focusArea.height = 375;
-              focusArea.width = 765;
-              console.log(`Area '${areaName}' resized to height: 375, width: 765`);
-          } else if (focusValue === "") {
-              focusArea.height = 0;
-              focusArea.width = 0;
-              console.log(`Area '${areaName}' resized to 0`);
+          if (areaName === 'focusArea') {
+              if (focusValue === "0" || focusValue === "1") {
+                  focusArea.height = 0;
+                  focusArea.width = 0;
+                  console.log(`Area '${areaName}' resized to 0`);
+              } else {
+                  focusArea.height = 375;
+                  focusArea.width = 765;
+                  console.log(`Area '${areaName}' resized to height: 375, width: 765`);
+              }
+          } else if (areaName === 'focusJitsi') {
+              if (focusValue === "0") {
+                  focusArea.height = 0;
+                  focusArea.width = 0;
+                  console.log(`Area '${areaName}' resized to 0`);
+              } else if (focusValue === "1" || focusValue === "0") {
+                  focusArea.height = 375;
+                  focusArea.width = 765;
+                  console.log(`Area '${areaName}' resized to height: 375, width: 765`);
+              }
           }
       }
   }
@@ -305,6 +317,7 @@ WA.onInit().then(async () => {
 
 
 ////////////////////////////////////////////////
+
 
 
 export {};
